5ed35f185d0f1cdec7f7f94653f150217e447965
Henry Fox <fengh16@163.com>
2024-03-17 16:14:19 +0800

    现在是下载所有关于graph的论文

--- download_latest.py
+++ download_latest.py
@@ -1,45 +1,88 @@
 import requests
-from time import time
+from time import time, sleep
 from xml.etree import ElementTree as ET
+from random import random
+from os import makedirs
 
 
 # get search term
-a = input("Whatcha wanna lookup? ")
+a = 'graph'
 
+step = 100  # 每一次取100篇
 
-# URL of the XML object
-url = "https://export.arxiv.org/api/query?search_query=all:%s&sortBy=lastUpdatedDate&sortOrder=descending&max_results=200" % a.lower().replace(' ','%20')
+start = 4700   # 从start开始
+timestamp = time()
+makedirs(f'output-{timestamp}', exist_ok=True)
+makedirs(f'raw-{timestamp}', exist_ok=True)
 
-# Send a GET request to the URL
-response = requests.get(url)
+while True:
+    # URL of the XML object
+    url = "http://export.arxiv.org/api/query?search_query=all:%s&sortBy=lastUpdatedDate&sortOrder=descending&start=%d&max_results=%d" % (a.lower().replace(' ','%20'), start, step)
+    print(url)
 
-# Parse the XML response
-root = ET.fromstring(response.content)
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0'
+        })
+    except:
+        print("出问题了，等一会")
+        sleep(1000 + random() * 2000)
+        continue
 
-# Namespace dictionary to find elements
-namespaces = {'atom': 'http://www.w3.org/2005/Atom', 'arxiv': 'http://arxiv.org/schemas/atom'}
+    # Parse the XML response
+    content_returned = response.content
+    root = ET.fromstring(content_returned)
+    with open(f"raw-{timestamp}\\{a}-{start}.xml", "wb") as file:
+        file.write(content_returned)
 
-# Open the output file with UTF-8 encoding
-with open("output-%s-%s.md" % (time(), a), "w", encoding='utf-8') as file:
-    # Iterate over each entry in the XML data
-    for entry in root.findall('atom:entry', namespaces):
-        # Extract and write the title
-        title = entry.find('atom:title', namespaces).text
-        title = ' '.join(title.split())  # Replace newlines and superfluous whitespace with a single space
-        file.write(f"# {title}\n\n")
+    # Namespace dictionary to find elements
+    namespaces = {'atom': 'http://www.w3.org/2005/Atom', 'arxiv': 'http://arxiv.org/schemas/atom'}
 
-        # Extract and write the link to the paper
-        id = entry.find('atom:id', namespaces).text
-        file.write(f"[Link to the paper]({id})\n\n")
+    get_number = 0
 
-        # Extract and write the authors
-        authors = entry.findall('atom:author', namespaces)
-        file.write("## Authors\n")
-        for author in authors:
-            name = author.find('atom:name', namespaces).text
-            file.write(f"- {name}\n")
-        file.write("\n")
+    # Open the output file with UTF-8 encoding
+    with open("output-%s\\%s-%d.md" % (timestamp, a, start), "w", encoding='utf-8') as file:
+        # Iterate over each entry in the XML data
+        for entry in root.findall('atom:entry', namespaces):
+            try:
+                # Extract and write the title
+                title = entry.find('atom:title', namespaces).text
+                title = ' '.join(title.split())  # Replace newlines and superfluous whitespace with a single space
+                file.write(f"# {title}\n\n")
 
-        # Extract and write the summary
-        summary = entry.find('atom:summary', namespaces).text
-        file.write(f"## Summary\n{summary}\n\n")
+                # Extract and write the link to the paper
+                id = entry.find('atom:id', namespaces).text
+                file.write(f"[Link to the paper]({id})\n\n")
+
+                # Extract and write the authors
+                authors = entry.findall('atom:author', namespaces)
+                file.write("## Authors\n")
+                for author in authors:
+                    name = author.find('atom:name', namespaces).text
+                    file.write(f"- {name}\n")
+                file.write("\n")
+
+                # Extract and write the summary
+                summary = entry.find('atom:summary', namespaces).text
+                file.write(f"## Summary\n{summary}\n\n")
+
+                get_number += 1
+            except:
+                pass
+    
+    if get_number == 0:
+        # 没有找到，可能超时了吧
+        print(content_returned.decode())
+        print(content_returned.decode().count("<summary>"))
+        print("超时了吧，休息一下吧")
+        sleep(1000 + random() * 2000)
+    elif get_number < step:
+        # 找完了
+        print(content_returned.decode())    
+        print(content_returned.decode().count("<summary>"))
+        print("找完了")
+        break
+    start += step
+    print("累计找到了%d篇" % start)
+    sleep(30 + random() * 900)  # 避免被封
